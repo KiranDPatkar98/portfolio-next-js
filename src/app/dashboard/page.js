de0bd8d92f596e5fd1f0fb2ff1f16406e982b6c7
Kpatkar <kiran.patkar@estuate.com>
2024-01-23 19:39:14 +0530

    created folder structure

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,9 @@
 import Link from 'next/link';
-
 export default function Home() {
   return (
     <>
-      <h1>Hi Kiran D Patkar</h1>
-      <Link href="/dashboard">Dashbaord</Link>
+      <h1>Iam dashbaord</h1>
+      <Link href="/"> Main page</Link>
     </>
   );
 }
