be3a0d341cd8f05ab5dc422c0252ce5a05924bd8
Kpatkar <kiran.patkar@estuate.com>
2024-01-26 17:34:28 +0530

    created sidebar component

--- src/app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-export default function Home() {
-  return (
-    <>
-      <h1>Iam dashbaord</h1>
-      <Link href="/"> Main page</Link>
-    </>
-  );
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
-import Link from 'next/link';
+'use client ';
+import Sidebar from '@/components/sidebar/Sidebar';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+// import { useState } from 'react';
 
 export default function Home() {
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
-    <>
-      <h1>Hi Kiran D Patkar</h1>
-      <Link href="/dashboard">Dashbaord</Link>
-    </>
+    <main>
+      <Container>
+        <Row>
+          <Col lg={3}>
+            <Sidebar />
+          </Col>
+          <Col lg={9}>
+            <h1>React Bootstrap NextJS Tutorial</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos aliquid quia optio odit nihil voluptatibus soluta
+              labore earum nostrum doloremque. Sequi laboriosam dicta
+              praesentium, sit aspernatur non molestiae voluptates beatae.
+            </p>
+            <Button>My Button</Button>
+          </Col>
+        </Row>
+      </Container>
+    </main>
   );
 }
